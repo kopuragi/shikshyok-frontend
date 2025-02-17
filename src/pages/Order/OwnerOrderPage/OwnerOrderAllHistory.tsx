4c20c6de2a3c4bbe321db5f7008322cbbffdae43
고민봉 <sjy2017z@naver.com>
2025-02-17 15:36:33 +0900

    주문내역UI

--- src/pages/Order/OwnerOrderPage/OwnerOrderAllHistory.tsx
+++ src/pages/Order/OwnerOrderPage/OwnerOrderAllHistory.tsx
@@ -1,6 +1,9 @@
-import "../../../styles/ownerOrderAllHistory.scss";
+import "../../../styles/ownerOrderHistory.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-interface OwnerOrderAllHistory {}
+interface OwnerOrderHistory {}
 
 const OwnerOrderAllHistory: React.FC = () => {
   return (
@@ -14,10 +17,13 @@ const OwnerOrderAllHistory: React.FC = () => {
             <p>전체 주문</p>
           </div>
         </div>
-
+        <hr className="border-4 border-black" />
         <div className="receipt-card-container-all">
           <div className="receipt-card-all">
             <ul className="receipt-card-list-all">
+              <li>
+                <FontAwesomeIcon icon={faTimes} className="custom-icon-all" />
+              </li>
               <li>주문시간</li>
               <li>[2025-02-15]</li>
               <li>19:30:55</li>
@@ -36,54 +42,25 @@ const OwnerOrderAllHistory: React.FC = () => {
               <li>방문시간</li>
               <li>19:40:55</li>
             </ul>
+            <div className="mt-2">
+              {window.innerWidth >= 480 ? (
+                <div>
+                  <button className="btn btn-warning">조리 시작</button>
+                  <button className="btn btn-success">조리 완료</button>
+                </div>
+              ) : (
+                <div>
+                  <button className="btn btn-warning btn-sm">조리 시작</button>
+                  <button className="btn btn-success btn-sm">조리 완료</button>
+                </div>
+              )}
+            </div>
           </div>
           <div className="receipt-card-all">
             <ul className="receipt-card-list-all">
-              <li>주문시간</li>
-              <li>[2025-02-15]</li>
-              <li>19:30:55</li>
-              <li>주문번호</li>
-              <li>123123</li>
-              <li>매장 인원 4명</li>
-              <li>연락처</li>
-              <li>010-1234-1234</li>
-              <li>메뉴이름</li>
-              <li>매우매우맛잇는치킨x1</li>
-              <li>매우매우맛잇는피자x1</li>
-              <li>매우매우맛잇는햄버거x1</li>
-              <li>매우매우맛잇는치킨x1</li>
-              <li>매우매우맛잇는피자x1</li>
-              <li>매우매우맛잇는햄버거x1</li>
-              <li>매우매우맛잇는치킨x1</li>
-              <li>매우매우맛잇는피자x1</li>
-              <li>매우매우맛잇는햄버거x1</li>
-              <br />
-              <li>합계: 85000원</li>
-              <li>방문시간</li>
-              <li>19:40:55</li>
-            </ul>
-          </div>
-          <div className="receipt-card-all">
-            <ul className="receipt-card-list-all">
-              <li>주문시간</li>
-              <li>[2025-02-15]</li>
-              <li>19:30:55</li>
-              <li>주문번호</li>
-              <li>123123</li>
-              <li>매장 인원 4명</li>
-              <li>연락처</li>
-              <li>010-1234-1234</li>
-              <li>메뉴이름</li>
-              <li>매우매우맛잇는햄버거x1</li>
-
-              <br />
-              <li>합계: 85000원</li>
-              <li>방문시간</li>
-              <li>19:40:55</li>
-            </ul>
-          </div>
-          <div className="receipt-card-all">
-            <ul className="receipt-card-list-all">
+              <li>
+                <FontAwesomeIcon icon={faTimes} className="custom-icon-all" />
+              </li>
               <li>주문시간</li>
               <li>[2025-02-15]</li>
               <li>19:30:55</li>
@@ -102,50 +79,22 @@ const OwnerOrderAllHistory: React.FC = () => {
               <li>방문시간</li>
               <li>19:40:55</li>
             </ul>
-          </div>
-          <div className="receipt-card-all">
-            <ul className="receipt-card-list-all">
-              <li>주문시간</li>
-              <li>[2025-02-15]</li>
-              <li>19:30:55</li>
-              <li>주문번호</li>
-              <li>123123</li>
-              <li>매장 인원 4명</li>
-              <li>연락처</li>
-              <li>010-1234-1234</li>
-              <li>메뉴이름</li>
-              <li>매우매우맛잇는치킨x1</li>
-
-              <br />
-              <li>합계: 85000원</li>
-              <li>방문시간</li>
-              <li>19:40:55</li>
-            </ul>
-          </div>
-          <div className="receipt-card-all">
-            <ul className="receipt-card-list-all">
-              <li>주문시간</li>
-              <li>[2025-02-15]</li>
-              <li>19:30:55</li>
-              <li>주문번호</li>
-              <li>123123</li>
-              <li>매장 인원 4명</li>
-              <li>연락처</li>
-              <li>010-1234-1234</li>
-              <li>메뉴이름</li>
-              <li>매우매우맛잇는치킨x1</li>
-              <li>매우매우맛잇는피자x1</li>
-              <li>매우매우맛잇는햄버거x1</li>
-
-              <br />
-              <li>합계: 85000원</li>
-              <li>방문시간</li>
-              <li>19:40:55</li>
-            </ul>
+            <div className="mt-2">
+              {window.innerWidth >= 480 ? (
+                <div>
+                  <button className="btn btn-warning">조리 시작</button>
+                  <button className="btn btn-success">조리 완료</button>
+                </div>
+              ) : (
+                <div>
+                  <button className="btn btn-warning btn-sm">조리 시작</button>
+                  <button className="btn btn-success btn-sm">조리 완료</button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </section>
-
       <nav className="pagination-container-all">
         <ul className="pagination-number-container-all">
           <li>
